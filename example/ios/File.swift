e0752c5f99ab9a58479df700542532cb68bdb3dd
Morgan <morgantrudeau@gmail.com>
2022-02-25 08:06:05 -0800

    Removed uneceassary code

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  AppbossCustomCodeExample
-//
-
-import Foundation
--- example/src/nativeComponents/LottieLoader.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import LottieView from 'lottie-react-native';
-
-const LottieLoader = () => {
-  return <LottieView source={require('./data/lottie.json')} autoPlay loop />;
-};
-
-export default LottieLoader;
