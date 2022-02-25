804480dadd6faad9fa4d9312b4240488cea1e064
Morgan <morgantrudeau@gmail.com>
2022-02-24 21:05:47 -0800

    Added native modules config

---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+import LottieLoader from './src/nativeComponents/LottieLoader';
+
+const LoginScreen = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'blue' }}>
+      <LottieLoader />
+    </View>
+  );
+};
+
+LoginScreen.navigationOptions = { header: null };
+
+export const applyCustomCode = (externalCodeSetup: any) => {
+  // call custom code api here
+
+  externalCodeSetup.navigationApi.replaceScreenComponent(
+    'LoginScreen',
+    LoginScreen
+  );
+};
+
+export { default as nativeModulesConfig } from './native_modules.config.js';
