8679b8a0cf841274baca870e29e36fae44a23447
Khelil Ben Osman <khelil.benosman@gmail.com>
2022-11-22 14:16:57 +0100

    config

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
+import React from "react";
+import { NativeModules } from "react-native";
+const { RNCustomCode } = NativeModules;
+
+import { initialize } from "@blocklienterprise/blockli";
+import config from "@src/build_config.json";
+
+export const applyCustomCode = async (externalCodeSetup) => {
+  const blockli_config = {
+    license: "EDSJ2PDI9BO5AKP", // Enter your 15 digit Blockli App Kit license key here. See your account dashboard at https://blockli.dev/dashboard
+    app_id: config.app_id,
+    code: externalCodeSetup,
+    website: "https://pole-masterclass.com", // Enter your app domain here with NO trailing slash.
+    // NOTE: If you have a staging site, please use a different Git branch for your test app and COPY this entire code to the test branch and enter the staging site domain.
+  };
+
+  await initialize(blockli_config);
 };
